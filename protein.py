d8d45c44bbd439380e28bf69b81e0df11db85188
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2023-01-27 14:31:10 +0530

    shifted to array type and added kearsleyand pairing

--- protein.py
+++ protein.py
@@ -1,12 +1,19 @@
 from proteinhelper import *
 L = []
 for i in range(10):
-    L.append(Point(np.random.randint(-5,5,size=3)))
+    L.append(np.random.randint(-5,5,size=3))
 
-protA = Protein(L)
+protA = Structure(np.array(L))
+protB = Structure(np.array(L))
+protB.translate([1,2,3])
+protB.rotate(np.random.randint(-5,5,size=3))
 
-protA.display('g')
-protA.translate(Vec([10,10,10]))
-protA.display('g')
-protA.rotate(Vec([0,0,1]),mt.pi)
-protA.display(col='g')
+pairing = Pairing([3,4,1,2,5,6,7,8,9,0],protA,protB)
+pairing.display('g','r')
+pairing.kearsley()
+pairing.display('g','r')
+# protA.display('r')
+# protA.translate([10,10,10])
+# protA.display('r')
+# protA.rotate(np.array([0,0,1])*mt.pi)
+# protA.display(col='r')
--- proteinhelper.py
+++ proteinhelper.py
@@ -1,50 +1,81 @@
 from vectors import *
 import matplotlib.pyplot as plt
 from kearsley import Kearsley
+from scipy.spatial.transform import Rotation as R
 
 k = Kearsley()
 
-class Point:
-    def __init__(self,coord,*args): #coord is a 1D array of 3 numbers
-        self.coord = Vec(coord)
-        if len(args) != 0:
-            self.label = args[0]
-    def translate(self,vector): #vector by which to translate (1D array)
-        self.coord.add(vector)
-    def rotate(self,axis,angle): #axis is a vector around which to rotate angle is in radians + when anticlockwise looking down in the direction opp to the axis
-        axis_cap = axis.cap()
-        A_vec = Vec(self.coord.vec - (self.coord.dot(axis_cap))*axis_cap.vec)
-        A1_vec = Vec((A_vec.mod()*mt.cos(angle))*A_vec.cap().vec + (A_vec.mod()*mt.sin(angle))*(A_vec.cross(axis_cap).vec))        
-        self.coord = Vec(A1_vec.vec + self.coord.dot(axis_cap)*axis_cap.vec)
+# class Point:
+#     def __init__(self,coord,*args): #coord is a 1D array of 3 numbers
+#         self.coord = Vec(coord)
+#         if len(args) != 0:
+#             self.label = args[0]
+#     def translate(self,vector): #vector by which to translate (1D array)
+#         self.coord.add(vector)
+#     def rotate(self,axis,angle): #axis is a vector around which to rotate angle is in radians + when anticlockwise looking down in the direction opp to the axis
+#         axis_cap = axis.cap()
+#         A_vec = Vec(self.coord.vec - (self.coord.dot(axis_cap))*axis_cap.vec)
+#         A1_vec = Vec((A_vec.mod()*mt.cos(angle))*A_vec.cap().vec + (A_vec.mod()*mt.sin(angle))*(A_vec.cross(axis_cap).vec))        
+#         self.coord = Vec(A1_vec.vec + self.coord.dot(axis_cap)*axis_cap.vec)
 
-class Protein:
-    def __init__(self,points): #points is a list of Point objects
+class Structure:
+    def __init__(self,points): #points is a nx3 array representing pointsin the structure
         self.points = points
     
-    def translate(self,vector):
-        for p in self.points:
-            p.translate(vector)
+    def translate(self,vector): #vector is a 1x3 array representing translation vector
+        n = self.points.shape[0]
+        L = [vector]*n
+        self.points += np.array(L)
+
     
-    def rotate(self,axis,angle):
-        for p in self.points:
-            p.rotate(axis,angle)
-        
+    def rotate(self,rotation_vec): #rotation_vec is a vector of axis of ratation whith magnitude = angle or rotation in radians
+        r = R.from_rotvec(rotation_vec)
+        self.points = r.apply(self.points) 
+
     def display(self,col):
         fig = plt.figure()
         ax = plt.axes(projection ='3d')
-        x,y,z = [], [], []
-        for p in self.points:
-            x.append(p.coord.vec[0])
-            y.append(p.coord.vec[1])
-            z.append(p.coord.vec[2])
-        xs = np.array(x)
-        ys = np.array(y)
-        zs = np.array(z)
+        x = self.points[:,0]
+        y = self.points[:,1]
+        z = self.points[:,2]
         ax.scatter(x,y,z,c=col)
         plt.show()
 
-class Pairings:
-    def __init__(self,A,B,P): #accepts structures A and B. P is dictionary that has key as points of A and val as points of B 
+
+class Pairing:
+    def __init__(self,permutation,A,B): #permutation is a list of size N containing integers 1 to N, such that L[i] = j means point i of A is paired to point j of B 
+        self.perm = permutation
         self.A = A
         self.B = B
-        
+    
+    def kearsley(self):
+        V = []
+        for i in range(self.B.points.shape[0]):
+            V.append(self.B.points[self.perm[i]])
+        V_arr = np.array(V)
+        U = self.A.points
+        k.fit(U,V_arr)
+        V_trans = k.transform(V_arr)
+        self.B.points = V_trans
+
+    def display(self,col1,col2):
+        fig = plt.figure()
+        ax = plt.axes(projection ='3d')
+        x = self.A.points[:,0]
+        y = self.A.points[:,1]
+        z = self.A.points[:,2]
+        ax.scatter(x,y,z,c=col1)
+        x = self.B.points[:,0]
+        y = self.B.points[:,1]
+        z = self.B.points[:,2]
+        ax.scatter(x,y,z,c=col2)
+        plt.show()
+
+
+
+    
+
+    
+    
+
+
